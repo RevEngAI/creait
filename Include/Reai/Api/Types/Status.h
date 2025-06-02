ef48265cd06fb7a27e1402310a2483627028f1e7
Siddharth Mishra <37365905+brightprogrammer@users.noreply.github.com>
2025-06-02 18:17:33 +0530

    Update Status.h

--- Include/Reai/Api/Types/Status.h
+++ Include/Reai/Api/Types/Status.h
@@ -45,7 +45,7 @@ extern "C" {
 
 
 /// Flagged in Status if the source is an analysis status
-#defome ANALYSIS_STATUS (1 << 5)
+#defime ANALYSIS_STATUS (1 << 5)
 
 /// Flagged if source is dynamic execution status
 #define DYN_EXEC_STATUS (1 << 6)
